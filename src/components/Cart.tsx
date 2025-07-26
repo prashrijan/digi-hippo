79e39b3e2a7405864caa4e6606118055ee703d42
Prashrijan <shresthaprashrijan@gmail.com>
2025-07-26 17:18:09 +1000

    nabar complete

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -4,14 +4,20 @@ import { ShoppingCart } from "lucide-react";
 import {
     Sheet,
     SheetContent,
+    SheetFooter,
     SheetHeader,
     SheetTitle,
     SheetTrigger,
 } from "./ui/sheet";
 import { Separator } from "./ui/separator";
+import { formatPrice } from "@/lib/utils";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import Image from "next/image";
 
 const Cart = () => {
-    const itemCount: number = 2;
+    const itemCount: number = 0;
+    const fee = 1;
 
     return (
         <Sheet>
@@ -43,20 +49,66 @@ const Cart = () => {
 
                             <div className="space-y-1.5 text-sm">
                                 <div className="flex">
-                                    <span className="flex-1">Shipping</span>
-                                    <span>Free</span>
+                                    <span className="flex-1 font-bold">
+                                        Shipping
+                                    </span>
+                                    <span className="text-primary">Free</span>
                                 </div>
                                 <div className="flex">
-                                    <span className="flex-1">
+                                    <span className="flex-1 font-bold">
                                         Transaction Fee
                                     </span>
-                                    <span>1</span>
+                                    <span>{formatPrice(fee)}</span>
+                                </div>
+                                <Separator />
+                                <div className="flex">
+                                    <span className="flex-1 font-bold">
+                                        Total
+                                    </span>
+                                    <span>{formatPrice(fee)}</span>
                                 </div>
                             </div>
+
+                            <SheetFooter>
+                                <SheetTrigger asChild>
+                                    <Link
+                                        href="/cart"
+                                        className={buttonVariants({
+                                            className: "w-full",
+                                        })}
+                                    >
+                                        Continue to Checkout
+                                    </Link>
+                                </SheetTrigger>
+                            </SheetFooter>
                         </div>
                     </>
                 ) : (
-                    <div></div>
+                    <div className="flex h-full flex-col items-center justify-center space-y-1">
+                        <div className="relative mb-4 h-60 w-60 text-muted-foreground">
+                            <Image
+                                src="/hippo-empty-cart.png"
+                                fill
+                                alt="empty shopping cart hippo"
+                            />
+                        </div>
+
+                        <div className="text-xl font-semibold">
+                            Your cart is empty
+                        </div>
+                        <SheetTrigger>
+                            <Link
+                                href="/prodcucts"
+                                className={buttonVariants({
+                                    variant: "link",
+                                    size: "sm",
+                                    className: "text-sm text-mutedforeground",
+                                })}
+                            >
+                                Add items to your cart to checkout
+                            </Link>
+                        </SheetTrigger>
+                    </div>
                 )}
             </SheetContent>
         </Sheet>
